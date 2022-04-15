dc50c62c2f0676f0e996640c630a50cba85bc86e
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-04-15 09:45:04 -0700

    fix: add tests for vrf error paths

--- tests/citycoin-vrf.test.ts
+++ tests/citycoin-vrf.test.ts
@@ -21,50 +21,71 @@ beforeEach(() => {
 });
 
 describe("[CityCoin VRF]", () => {
-  describe("VRF CONSISTENCY", () => {
-    it("succeeds and returns the same value from v1 and v2", () => {
-      // arrange
-      const blocks = 10;
-      // act
-      const block_1 = chain.mineEmptyBlock(blocks).block_height - 1;
-      const resultV1_1 = vrf.getRandomUintAtBlock(block_1).result;
-      const resultV2_1 = vrfV2.getRnd(block_1).result;
-
-      const block_2 = chain.mineEmptyBlock(blocks * 2).block_height - 1;
-      const resultV1_2 = vrf.getRandomUintAtBlock(block_2).result;
-      const resultV2_2 = vrfV2.getRnd(block_2).result;
-
-      const block_3 = chain.mineEmptyBlock(blocks * 3).block_height - 1;
-      const resultV1_3 = vrf.getRandomUintAtBlock(block_3).result;
-      const resultV2_3 = vrfV2.getRnd(block_3).result;
-      // assert
-      assertEquals(resultV1_1.expectSome(), resultV2_1.expectOk());
-      assertEquals(resultV1_2.expectSome(), resultV2_2.expectOk());
-      assertEquals(resultV1_3.expectSome(), resultV2_3.expectOk());
-    });
-
-    it("succeeds and returns the same value from both V2 methods", () => {
-      // arrange
-      const user = accounts.get("wallet_1")!;
-      const blocks = 10;
-      // act
-      const block_1 = chain.mineEmptyBlock(blocks).block_height - 1;
-      const receipt_1 = chain.mineBlock([
-        vrfV2.getSaveRnd(block_1, user),
-      ]).receipts[0];
-      const receipt_2 = chain.mineBlock([
-        vrfV2.getSaveRnd(block_1, user),
-      ]).receipts[0];
-      const result_3 = vrfV2.getRnd(block_1).result;
-      // assert
-      console.log(receipt_1, receipt_2);
-      assertEquals(
-        receipt_1.result.expectOk(),
-        receipt_2.result.expectOk(),
-        result_3
-      );
-    });
+  it("V1 succeeds and returns none if block height is in the future", () => {
+    // arrange
+    const futureBlock = 10;
+    // act
+    const result = vrf.getRandomUintAtBlock(futureBlock).result;
+    // assert
+    result.expectNone();
   });
+
+  it("V2 fails with ERR_FAIL if block height is in the future", () => {
+    // note: getSaveRnd test disabled due to error in Clarinet
+    // arrange
+    // const user = accounts.get("wallet_1")!;
+    const futureBlock = 10;
+    // act
+    const result = vrfV2.getRnd(futureBlock).result;
+    // const receipt = chain.mineBlock([
+    //   vrfV2.getSaveRnd(futureBlock, user),
+    // ]).receipts[0];
+    // assert
+    result.expectErr().expectUint(VrfModelV2.ErrCode.ERR_FAIL);
+  });
+
+  it("succeeds and returns the same value from v1 and v2", () => {
+    // arrange
+    const blocks = 10;
+    // act
+    const block_1 = chain.mineEmptyBlock(blocks).block_height - 1;
+    const resultV1_1 = vrf.getRandomUintAtBlock(block_1).result;
+    const resultV2_1 = vrfV2.getRnd(block_1).result;
+
+    const block_2 = chain.mineEmptyBlock(blocks * 2).block_height - 1;
+    const resultV1_2 = vrf.getRandomUintAtBlock(block_2).result;
+    const resultV2_2 = vrfV2.getRnd(block_2).result;
+
+    const block_3 = chain.mineEmptyBlock(blocks * 3).block_height - 1;
+    const resultV1_3 = vrf.getRandomUintAtBlock(block_3).result;
+    const resultV2_3 = vrfV2.getRnd(block_3).result;
+    // assert
+    assertEquals(resultV1_1.expectSome(), resultV2_1.expectOk());
+    assertEquals(resultV1_2.expectSome(), resultV2_2.expectOk());
+    assertEquals(resultV1_3.expectSome(), resultV2_3.expectOk());
+  });
+
+  it("succeeds and returns the same value from both V2 methods", () => {
+    // arrange
+    const user = accounts.get("wallet_1")!;
+    const blocks = 10;
+    // act
+    const block_1 = chain.mineEmptyBlock(blocks).block_height - 1;
+    const receipt_1 = chain.mineBlock([
+      vrfV2.getSaveRnd(block_1, user),
+    ]).receipts[0];
+    const receipt_2 = chain.mineBlock([
+      vrfV2.getSaveRnd(block_1, user),
+    ]).receipts[0];
+    const result_3 = vrfV2.getRnd(block_1).result;
+    // assert
+    assertEquals(
+      receipt_1.result.expectOk(),
+      receipt_2.result.expectOk(),
+      result_3
+    );
+  });
+
 });
 
 run();
