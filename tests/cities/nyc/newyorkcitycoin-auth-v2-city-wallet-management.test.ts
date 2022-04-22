b9da729b32de6da148bebe4a4b612f53d5b48b8a
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-04-22 14:08:00 -0700

    feat: add v2 auth contracts, models, and tests
    Note that a number of tests were skipped as they require interaction with a new core contract. Given the size of this PR already, it would be better to build a 2nd PR from here with those changes and anything else that finishes up what's needed in the tracking issue.

---
+++ tests/cities/nyc/newyorkcitycoin-auth-v2-city-wallet-management.test.ts
@@ -0,0 +1,178 @@
+import { describe, run, Chain, beforeEach, it} from "../../../deps.ts";
+import { NewYorkCityCoinAuthModelV2 } from "../../../models/newyorkcitycoin-auth-v2.model.ts";
+import { NewYorkCityCoinCoreModel } from "../../../models/newyorkcitycoin-core.model.ts";
+import { Accounts, Context } from "../../../src/context.ts";
+
+let ctx: Context;
+let chain: Chain;
+let accounts: Accounts;
+let authV2: NewYorkCityCoinAuthModelV2;
+let core: NewYorkCityCoinCoreModel;
+
+beforeEach(() => {
+  ctx = new Context();
+  chain = ctx.chain;
+  accounts = ctx.accounts;
+  authV2 = ctx.models.get(NewYorkCityCoinAuthModelV2, "newyorkcitycoin-auth-v2");
+  core = ctx.models.get(NewYorkCityCoinCoreModel, "newyorkcitycoin-core-v1");
+})
+
+describe("[NewYorkCityCoin Auth v2]", () => {
+  //////////////////////////////////////////////////
+  // CITY WALLET MANAGEMENT
+  //////////////////////////////////////////////////
+  describe("CITY WALLET MANAGEMENT", () => {
+    describe("get-city-wallet()", () => {
+      it("succeeds and returns city wallet", () => {
+        // arrange
+        const cityWallet = accounts.get("nyc_wallet")!;
+        // act
+        const result = authV2.getCityWallet().result;
+        // assert
+        result.expectOk().expectPrincipal(cityWallet.address);
+      });
+    });
+    describe("set-city-wallet()", () => {
+      it("fails with ERR_CORE_CONTRACT_NOT_FOUND if principal not found in core contracts map", () => {
+        // arrange
+        const cityWallet = accounts.get("nyc_wallet")!;
+        const newCityWallet = accounts.get("wallet_2")!;
+        // act
+        const receipt = chain.mineBlock([
+          authV2.setCityWallet(
+            core.address,
+            newCityWallet,
+            cityWallet
+          ),
+        ]).receipts[0];
+        // assert
+        receipt.result
+          .expectErr()
+          .expectUint(NewYorkCityCoinAuthModelV2.ErrCode.ERR_CORE_CONTRACT_NOT_FOUND);
+      });
+
+      it.skip("fails with ERR_UNAUTHORIZED if not called by city wallet", () => {
+        // arrange
+        const sender = accounts.get("wallet_1")!;
+        const newCityWallet = accounts.get("wallet_2")!;
+        chain.mineBlock([
+          core.testInitializeCore(core.address),
+        ]);
+        // act
+        const receipt = chain.mineBlock([
+          authV2.setCityWallet(
+            core.address,
+            newCityWallet,
+            sender
+          ),
+        ]).receipts[0];
+        // assert
+        receipt.result
+          .expectErr()
+          .expectUint(NewYorkCityCoinAuthModelV2.ErrCode.ERR_UNAUTHORIZED);
+      });
+
+      it.skip("fails with ERR_UNAUTHORIZED if not called by the active core contract", () => {
+        // arrange
+        const cityWallet = accounts.get("nyc_wallet")!;
+        const newCityWallet = accounts.get("wallet_2")!;
+        chain.mineBlock([
+          core.testInitializeCore(core.address),
+        ]);
+        // act
+        const receipt = chain.mineBlock([
+          authV2.setCityWallet(
+            core.address,
+            newCityWallet,
+            cityWallet
+          ),
+        ]).receipts[0];
+        // assert
+        receipt.result
+          .expectErr()
+          .expectUint(NewYorkCityCoinAuthModelV2.ErrCode.ERR_UNAUTHORIZED);
+      });
+
+      it.skip("succeeds and updates city wallet variable when called by current city wallet", () => {
+        // arrange
+        const cityWallet = accounts.get("nyc_wallet")!;
+        const newCityWallet = accounts.get("wallet_2")!;
+        chain.mineBlock([
+          core.testInitializeCore(core.address),
+          authV2.testSetActiveCoreContract(cityWallet),
+        ]);
+
+        // act
+        const receipt = chain.mineBlock([
+          authV2.setCityWallet(
+            core.address,
+            newCityWallet,
+            cityWallet
+          ),
+        ]).receipts[0];
+
+        // assert
+        receipt.result.expectOk().expectBool(true);
+        core
+          .getCityWallet()
+          .result.expectPrincipal(newCityWallet.address);
+        authV2
+          .getCityWallet()
+          .result.expectOk()
+          .expectPrincipal(newCityWallet.address);
+      });
+    });
+    describe("execute-set-city-wallet-job()", () => {
+      it.skip("succeeds and updates city wallet variable when called by job approver", () => {
+        // arrange
+        const jobId = 1;
+        const sender = accounts.get("wallet_1")!;
+        const approver1 = accounts.get("wallet_2")!;
+        const approver2 = accounts.get("wallet_3")!;
+        const approver3 = accounts.get("wallet_4")!;
+        const cityWallet = accounts.get("nyc_wallet")!;
+        const newCityWallet = accounts.get("wallet_2")!;
+        chain.mineBlock([
+          core.testInitializeCore(core.address),
+          authV2.testSetActiveCoreContract(cityWallet),
+        ]);
+
+        chain.mineBlock([
+          authV2.createJob(
+            "update city wallet 1",
+            authV2.address,
+            sender
+          ),
+          authV2.addPrincipalArgument(
+            jobId,
+            "newCityWallet",
+            newCityWallet.address,
+            sender
+          ),
+          authV2.activateJob(jobId, sender),
+          authV2.approveJob(jobId, approver1),
+          authV2.approveJob(jobId, approver2),
+          authV2.approveJob(jobId, approver3),
+        ]);
+
+        // act
+        const receipt = chain.mineBlock([
+          authV2.executeSetCityWalletJob(
+            jobId,
+            core.address,
+            approver1
+          ),
+        ]).receipts[0];
+
+        // asserts
+        receipt.result.expectOk().expectBool(true);
+
+        core
+          .getCityWallet()
+          .result.expectPrincipal(newCityWallet.address);
+      });
+    });
+  });
+});
+
+run();
---
+++ tests/cities/nyc/newyorkcitycoin-auth-v2-token-management.test.ts
@@ -0,0 +1,209 @@
+import { describe, run, Chain, beforeEach, it} from "../../../deps.ts";
+import { NewYorkCityCoinAuthModelV2 } from "../../../models/newyorkcitycoin-auth-v2.model.ts";
+import { NewYorkCityCoinTokenModel } from "../../../models/newyorkcitycoin-token.model.ts";
+import { Accounts, Context } from "../../../src/context.ts";
+
+let ctx: Context;
+let chain: Chain;
+let accounts: Accounts;
+let authV2: NewYorkCityCoinAuthModelV2;
+let token: NewYorkCityCoinTokenModel;
+
+beforeEach(() => {
+  ctx = new Context();
+  chain = ctx.chain;
+  accounts = ctx.accounts;
+  authV2 = ctx.models.get(NewYorkCityCoinAuthModelV2, "newyorkcitycoin-auth-v2");
+  token = ctx.models.get(NewYorkCityCoinTokenModel, "newyorkcitycoin-token");
+})
+
+describe("[NewYorkCityCoin Auth v2]", () => {
+  //////////////////////////////////////////////////
+  // TOKEN MANAGEMENT
+  //////////////////////////////////////////////////
+  describe("TOKEN MANAGEMENT", () => {
+    describe("set-token-uri()", () => {
+      it("fails with ERR_UNAUTHORIZED when called by someone who is not city wallet", () => {
+        // arrange
+        const sender = accounts.get("wallet_2")!;
+        // act
+        const block = chain.mineBlock([
+          authV2.setTokenUri(
+            sender,
+            token.address,
+            "http://something-something.com"
+          ),
+        ]);
+        // assert
+        const receipt = block.receipts[0];
+
+        receipt.result
+          .expectErr()
+          .expectUint(NewYorkCityCoinAuthModelV2.ErrCode.ERR_UNAUTHORIZED);
+      });
+      it("fails with ERR_UNAUTHORIZED when called by someone who is not auth contract", () => {
+        // arrange
+        const sender = accounts.get("wallet_2")!;
+        // act
+        const block = chain.mineBlock([
+          token.setTokenUri(sender, "http://something-something.com"),
+        ]);
+        // assert
+        const receipt = block.receipts[0];
+
+        receipt.result
+          .expectErr()
+          .expectUint(NewYorkCityCoinTokenModel.ErrCode.ERR_UNAUTHORIZED);
+      });
+      it.skip("succeeds and updates token uri to none if no new value is provided", () => {
+        // arrange
+        const sender = accounts.get("nyc_wallet")!;
+        // act
+        const block = chain.mineBlock([
+          authV2.setTokenUri(sender, token.address),
+        ]);
+        // assert
+        const receipt = block.receipts[0];
+
+        receipt.result.expectOk().expectBool(true);
+
+        const result = token.getTokenUri().result;
+        result.expectOk().expectNone();
+      });
+      it.skip("succeeds and updates token uri to new value if provided", () => {
+        // arrange
+        const sender = accounts.get("nyc_wallet")!;
+        const newUri = "http://something-something.com";
+        // act
+        const block = chain.mineBlock([
+          authV2.setTokenUri(
+            sender,
+            token.address,
+            newUri
+          ),
+        ]);
+        // assert
+        const receipt = block.receipts[0];
+
+        receipt.result.expectOk().expectBool(true);
+
+        const result = token.getTokenUri().result;
+        result.expectOk().expectSome().expectUtf8(newUri);
+      });
+    });
+  });
+
+  describe("APPROVERS MANAGEMENT", () => {
+    describe("execute-replace-approver-job()", () => {
+      it("succeeds and replaces one approver with a new principal", () => {
+        const jobId = 1;
+        const approver1 = accounts.get("wallet_1")!;
+        const approver2 = accounts.get("wallet_2")!;
+        const approver3 = accounts.get("wallet_3")!;
+        const approver4 = accounts.get("wallet_4")!;
+        const newApprover = accounts.get("wallet_7")!;
+
+        authV2.isApprover(newApprover).result.expectBool(false);
+        chain.mineBlock([
+          authV2.createJob(
+            "replace approver1",
+            authV2.address,
+            approver1
+          ),
+          authV2.addPrincipalArgument(
+            jobId,
+            "oldApprover",
+            approver1.address,
+            approver1
+          ),
+          authV2.addPrincipalArgument(
+            jobId,
+            "newApprover",
+            newApprover.address,
+            approver1
+          ),
+          authV2.activateJob(jobId, approver1),
+          authV2.approveJob(jobId, approver1),
+          authV2.approveJob(jobId, approver2),
+          authV2.approveJob(jobId, approver3),
+          authV2.approveJob(jobId, approver4),
+        ]);
+
+        const receipt = chain.mineBlock([
+          authV2.executeReplaceApproverJob(jobId, approver1),
+        ]).receipts[0];
+
+        receipt.result.expectOk().expectBool(true);
+
+        authV2.isApprover(approver1).result.expectBool(false);
+        authV2.isApprover(newApprover).result.expectBool(true);
+      });
+
+      it("fails with ERR_UNAUTHORIZED if replaced/inactive approver creates or approves jobs", () => {
+        const replaceApproverJobId = 1;
+        const anotherJobId = 2;
+        const oldApprover = accounts.get("wallet_1")!;
+        const approver2 = accounts.get("wallet_2")!;
+        const approver3 = accounts.get("wallet_3")!;
+        const approver4 = accounts.get("wallet_4")!;
+        const newApprover = accounts.get("wallet_7")!;
+
+        authV2.isApprover(newApprover).result.expectBool(false);
+        chain.mineBlock([
+          authV2.createJob(
+            "replace oldApprover",
+            authV2.address,
+            approver2
+          ),
+          authV2.addPrincipalArgument(
+            replaceApproverJobId,
+            "oldApprover",
+            oldApprover.address,
+            approver2
+          ),
+          authV2.addPrincipalArgument(
+            replaceApproverJobId,
+            "newApprover",
+            newApprover.address,
+            approver2
+          ),
+          authV2.activateJob(replaceApproverJobId, approver2),
+          authV2.approveJob(replaceApproverJobId, oldApprover),
+          authV2.approveJob(replaceApproverJobId, approver2),
+          authV2.approveJob(replaceApproverJobId, approver3),
+          authV2.approveJob(replaceApproverJobId, approver4),
+          authV2.executeReplaceApproverJob(
+            replaceApproverJobId,
+            oldApprover
+          ),
+          authV2.createJob(
+            "new job",
+            authV2.address,
+            approver2
+          ),
+          authV2.activateJob(anotherJobId, approver2),
+        ]);
+
+        // act
+        const receipts = chain.mineBlock([
+          authV2.createJob(
+            "test job",
+            authV2.address,
+            oldApprover
+          ),
+          authV2.approveJob(anotherJobId, oldApprover),
+        ]).receipts;
+
+        // assert
+        receipts[0].result
+          .expectErr()
+          .expectUint(NewYorkCityCoinAuthModelV2.ErrCode.ERR_UNAUTHORIZED);
+        receipts[1].result
+          .expectErr()
+          .expectUint(NewYorkCityCoinAuthModelV2.ErrCode.ERR_UNAUTHORIZED);
+      });
+    });
+  });
+});
+
+run();
