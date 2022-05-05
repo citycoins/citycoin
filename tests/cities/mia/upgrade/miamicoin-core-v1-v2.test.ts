208886abc421c9cdb0e4e3da3260c9f75abc76c1
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-05-04 18:09:30 -0700

    fix: add tests for core v1 and v2 functions after upgrade

--- tests/cities/mia/upgrade/miamicoin-core-v1-v2.test.ts
+++ tests/cities/mia/upgrade/miamicoin-core-v1-v2.test.ts
@@ -1,7 +1,6 @@
 import { assertEquals, describe, run, Chain, beforeEach, it, Account, types } from "../../../../deps.ts";
 import { Accounts, Context } from "../../../../src/context.ts";
 import { MiamiCoinAuthModel } from "../../../../models/cities/mia/miamicoin-auth.model.ts";
-import { MiamiCoinAuthModelV2 } from "../../../../models/cities/mia/miamicoin-auth-v2.model.ts";
 import { MiamiCoinCoreModel } from "../../../../models/cities/mia/miamicoin-core.model.ts";
 import { MiamiCoinCoreModelPatch } from "../../../../models/cities/mia/miamicoin-core-v1-patch.model.ts";
 import { MiamiCoinCoreModelV2 } from "../../../../models/cities/mia/miamicoin-core-v2.model.ts";
@@ -12,7 +11,6 @@ let ctx: Context;
 let chain: Chain;
 let accounts: Accounts;
 let auth: MiamiCoinAuthModel;
-let authV2: MiamiCoinAuthModelV2;
 let core: MiamiCoinCoreModel;
 let coreV1Patch: MiamiCoinCoreModelPatch;
 let coreV2: MiamiCoinCoreModelV2;
@@ -24,7 +22,6 @@ beforeEach(() => {
   chain = ctx.chain;
   accounts = ctx.accounts;
   auth = ctx.models.get(MiamiCoinAuthModel, "miamicoin-auth");
-  authV2 = ctx.models.get(MiamiCoinAuthModelV2, "miamicoin-auth-v2");
   core = ctx.models.get(MiamiCoinCoreModel, "miamicoin-core-v1");
   coreV1Patch = ctx.models.get(MiamiCoinCoreModelPatch, "miamicoin-core-v1-patch");
   coreV2 = ctx.models.get(MiamiCoinCoreModelV2, "miamicoin-core-v2");
@@ -43,6 +40,7 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
   let user1: Account;
   let user2: Account;
   let user3: Account;
+  let cityWallet: Account;
 
   beforeEach(() => {
     // setup accounts
@@ -53,8 +51,10 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
     user1 = accounts.get("wallet_6")!;
     user2 = accounts.get("wallet_7")!;
     user3 = accounts.get("wallet_8")!;
+    cityWallet = accounts.get("mia_wallet")!;
 
     // activate core v1
+    chain.mineEmptyBlockUntil(MiamiCoinCoreModelV2.MIAMICOIN_ACTIVATION_HEIGHT - 1);
     const setupBlock = chain.mineBlock([
       token.testMint(mintAmount, user1),
       token.testMint(mintAmount, user2),
@@ -68,37 +68,53 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
 
     // mine tokens for claim in past
     // console.log("---------- Mining")
-    // console.log(`block: 152, user1`)
+    // console.log(`block: 24646, user1`)
     chain.mineBlock([
       core.mineTokens(minerCommit * 1000, user1),
       core.mineTokens(minerCommit, user2),
       core.mineTokens(minerCommit, user3),
     ]);
     chain.mineEmptyBlock(100);
-    // console.log(`block: 253, user2`)
+    // console.log(`block: 24747, user2`)
     chain.mineBlock([
       core.mineTokens(minerCommit, user1),
       core.mineTokens(minerCommit * 1000, user2),
       core.mineTokens(minerCommit, user3),
     ]);
     chain.mineEmptyBlock(100);
-    // console.log(`block: 354, user3`)
+    // console.log(`block: 24848, user3`)
     chain.mineBlock([
       core.mineTokens(minerCommit, user1),
       core.mineTokens(minerCommit, user2),
       core.mineTokens(minerCommit * 1000, user3),
     ]);
 
-    // fast-forward to MIA cycle 12 (49697)
+    // fast-forward to MIA cycle 10 (45497)
     // stack tokens for testing claims
     // before during and after shutdown
-    chain.mineEmptyBlockUntil(49699);
+    chain.mineEmptyBlockUntil(46000);
     // console.log("---------- Stacking")
-    // console.log(`block: 49,700`)
+    // console.log(`block: 46,000`)
     chain.mineBlock([
-      core.stackTokens(1000, 4, user1),  // cycles 12 - 15
-      core.stackTokens(1000, 5, user2),  // cycles 12 - 16
-      core.stackTokens(1000, 10, user3), // cycles 12 - 22
+      core.stackTokens(1000, 3, user1),  // cycles 11 - 13
+      core.stackTokens(1000, 6, user2),  // cycles 11 - 16
+      core.stackTokens(1000, 10, user3), // cycles 11 - 20
+    ]);
+
+    // mine in MIA cycle 11 after tokens are stacked
+    chain.mineEmptyBlock(MiamiCoinCoreModelV2.REWARD_CYCLE_LENGTH);
+    chain.mineBlock([
+      core.mineTokens(minerCommit * 1000, user1),
+      core.mineTokens(minerCommit, user2),
+      core.mineTokens(minerCommit, user3),
+    ]);
+
+    // mine in MIA cycle 16 before the upgrade (58097)
+    chain.mineEmptyBlockUntil(58100);
+    chain.mineBlock([
+      core.mineTokens(minerCommit * 1000, user1),
+      core.mineTokens(minerCommit, user2),
+      core.mineTokens(minerCommit, user3),
     ]);
 
     // fast-forward to just before the upgrade
@@ -141,7 +157,7 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
     // fast-forward to proposed shutdown height
     // and setup upgrade using approvers job
     // citycoin-core-v1 -> citycoin-core-v1-patch
-    chain.mineEmptyBlockUntil(58999);
+    chain.mineEmptyBlockUntil(59000);
     
     // perform contract upgrade and activate
     // new core-v2 contract for mining/stacking
@@ -162,6 +178,10 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
     chain.mineEmptyBlockUntil(activationBlockUpgrade);
   });
 
+  /////////////////////////
+  // register-user()
+  /////////////////////////
+
   it("register-user() succeeded in v2 and set correct activation block", () => {
     // act
     const coreV2Activation = coreV2.getActivationBlock().result;
@@ -176,7 +196,6 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
     const epochLength = MiamiCoinCoreModelV2.TOKEN_EPOCH_LENGTH;
     // act
     const result = coreV2.getCoinbaseThresholds().result;
-    console.log(`result: ${result}`)
     // assert
     const expectedResult = {
       coinbaseThreshold1: types.uint(activationBlock + bonusPeriod + epochLength),      // 59497
@@ -188,55 +207,277 @@ describe("[MiamiCoin Core Upgrade v1-v2]", () => {
     assertEquals(result.expectOk().expectTuple(), expectedResult);
   });
 
-  // MIA activation: 24497
+  it("register-user() succeeds and sets correct coinbase amounts", () => {
+    // arrange
+    const user = accounts.get("wallet_1")!;
+    const microCitycoins = MiamiCoinCoreModelV2.MICRO_CITYCOINS;
+    const block = chain.mineBlock([
+      coreV2.testInitializeCore(coreV2.address),
+      coreV2.testSetActivationThreshold(1),
+      coreV2.registerUser(user)
+    ]);
+    const activationBlockHeight =
+      block.height + MiamiCoinCoreModelV2.ACTIVATION_DELAY - 1;
+    chain.mineEmptyBlockUntil(activationBlockHeight);
+    // act
+    const result = coreV2.getCoinbaseAmounts().result;
+    // assert
+    const expectedResult = {
+      coinbaseAmount1: types.uint(100000 * microCitycoins),
+      coinbaseAmount2: types.uint(50000 * microCitycoins),
+      coinbaseAmount3: types.uint(25000 * microCitycoins),
+      coinbaseAmount4: types.uint(12500 * microCitycoins),
+      coinbaseAmount5: types.uint(6250 * microCitycoins),
+      coinbaseAmountBonus: types.uint(250000 * microCitycoins),
+      coinbaseAmountDefault: types.uint(3125 * microCitycoins),
+    };
+    assertEquals(result.expectOk().expectTuple(), expectedResult);
+  });
 
-  // epoch 1: 24497  + 10000  = 34497  (100k)
-  // epoch 2: 34497  + 25000  = 59497  (50k)
-  // epoch 3: 54497  + 50000  = 109497 (25k)
-  // epoch 4: 109497 + 100000 = 209497 (12.5k)
-  // epoch 5: 209497 + 200000 = 409497 (6.25k)
-  // epoch 6: 409497 + 400000 = 809497 (3.125k)
+  /////////////////////////
+  // mine-tokens()
+  /////////////////////////
 
-  // epoch 1: 24497 + 10000                                              = 34497
-  // epoch 2: 24497 + 10000 + 25000                                      = 59497
-  // epoch 3: 24497 + 10000 + (25000 + 50000)                            = 109497
-  // epoch 4: 24497 + 10000 + (25000 + 50000 + 100000)                   = 209497
-  // epoch 5: 24497 + 10000 + (25000 + 50000 + 100000 + 200000)          = 409497
-  // epoch 6: 24497 + 10000 + (25000 + 50000 + 100000 + 200000 + 400000) = 809497
+  it("mine-tokens() fails in v1 with ERR_CONTRACT_NOT_ACTIVATED after upgrade", () => {
+    // act
+    const receipt = chain.mineBlock([
+      core.mineTokens(minerCommit, user1),
+    ]).receipts[0];
+    // assert
+    receipt.result.expectErr().expectUint(MiamiCoinCoreModelV2.ErrCode.ERR_CONTRACT_NOT_ACTIVATED);
+  });
 
-  // epoch 1: 24497 + 10000              = 34497
-  // epoch 2: 24497 + 10000 + 25000      = 59497  (+ 25000)
-  // epoch 3: 24497 + 10000 + 25000 * 3  = 109497 (+ 50000)
-  // epoch 4: 24497 + 10000 + 25000 * 7  = 209497 (+ 100000)
-  // epoch 5: 24497 + 10000 + 25000 * 15 = 409497 (+ 200000)
-  // epoch 6: 24497 + 10000 + 25000 * 31 = 809497 (+ 400000)
+  it("mine-tokens() succeeds in v2 contract after upgrade and intialization", () => {
+    // act
+    const receipt = chain.mineBlock([
+      coreV2.mineTokens(minerCommit, user1),
+    ]).receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 1);
+    receipt.events.expectSTXTransferEvent(
+      minerCommit,
+      user1.address,
+      cityWallet.address
+    );
+  });
 
+  /////////////////////////
+  // mine-many()
+  /////////////////////////
+
+  it("mine-many() fails in v1 with ERR_CONTRACT_NOT_ACTIVATED after upgrade", () => {
+    // arrange
+    const minerCommits = new Array(100).fill(minerCommit * 100);
+    // act
+    const receipt = chain.mineBlock([
+      core.mineMany(minerCommits, user1),
+    ]).receipts[0];
+    // assert
+    receipt.result.expectErr().expectUint(MiamiCoinCoreModelV2.ErrCode.ERR_CONTRACT_NOT_ACTIVATED);
+  });
+
+  it("mine-many() succeeds in v2 contract after upgrade and intialization", () => {
+    // arrange
+    const minerCommits = new Array(100).fill(minerCommit * 100);
+    // act
+    const block = chain.mineBlock([
+      coreV2.mineMany(minerCommits, user1),
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 2);
+    receipt.events.expectSTXTransferEvent(
+      minerCommit * 10000,
+      user1.address,
+      cityWallet.address
+    );
+  });
+
+  /////////////////////////
+  // stack-tokens()
+  /////////////////////////
+
+  it("stack-tokens() succeeds in v2 contract starting at cycle shutdown occurred in", () => {
+    // arrange
+    const amountStacked = 200;
+    const lockPeriod = 5;
+    chain.mineBlock([
+      tokenV2.convertToV2(user1),
+    ]);
+    // act
+    const block = chain.mineBlock([
+      coreV2.stackTokens(amountStacked, lockPeriod, user1),
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 2);
+    receipt.events.expectFungibleTokenTransferEvent(
+      amountStacked,
+      user1.address,
+      coreV2.address,
+      "miamicoin"
+    );
+  });
+
+  it("stack-tokens() succeeds in v2 contract after v1 shutdown occurred", () => {
+    // arrange
+    chain.mineEmptyBlock(MiamiCoinCoreModelV2.REWARD_CYCLE_LENGTH)
+    const amountStacked = 200;
+    const lockPeriod = 5;
+    chain.mineBlock([
+      tokenV2.convertToV2(user1),
+    ]);
+    // act
+    const block = chain.mineBlock([
+      coreV2.stackTokens(amountStacked, lockPeriod, user1),
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 2);
+    receipt.events.expectFungibleTokenTransferEvent(
+      amountStacked,
+      user1.address,
+      coreV2.address,
+      "miamicoin"
+    );
+  });
+
+  /////////////////////////
+  // claim-mining-reward()
+  /////////////////////////
+
+  it("claim-mining-reward() fails in v1 with ERR_CLAIM_IN_WRONG_CONTRACT after shutdown block", () => {
+    // arrange
+    const targetBlock = 59000;
+    // act
+    const block = chain.mineBlock([
+      core.claimMiningReward(targetBlock, user1),
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectErr().expectUint(MiamiCoinCoreModelV2.ErrCode.ERR_CLAIM_IN_WRONG_CONTRACT);
+  });
+
+  it("claim-mining-reward() succeeds in v1 with block height in the past", () => {
+    // arrange
+    const targetBlock1 = 24646;
+    const targetBlock2 = 24747;
+    const targetBlock3 = 24848;
+    // act
+    const block = chain.mineBlock([
+      core.claimMiningReward(targetBlock1, user1),
+      core.claimMiningReward(targetBlock2, user2),
+      core.claimMiningReward(targetBlock3, user3),
+    ]);
+    // assert
+    block.receipts[0].result.expectOk().expectBool(true);
+    block.receipts[1].result.expectOk().expectBool(true);
+    block.receipts[2].result.expectOk().expectBool(true);
+  });
+
+  it("claim-mining-reward() succeeds in v1 with block prior to end of the upgrade", () => {
+    // arrange
+    const targetBlock1 = 58998;
+    const targetBlock2 = 58999;
+    // act
+    const block = chain.mineBlock([
+      core.claimMiningReward(targetBlock1, user1),
+      core.claimMiningReward(targetBlock2, user1),
+    ]);
+    // assert
+    block.receipts[0].result.expectOk().expectBool(true);
+    block.receipts[1].result.expectOk().expectBool(true);
+  });
+
+  /////////////////////////
+  // claim-stacking-reward()
+  /////////////////////////
+
+  it("claim-stacking-reward() succeeds in v1 with reward cycle in the past after shutdown", () => {
+    // arrange
+    const targetCycle = 11;
+    // act
+    const block = chain.mineBlock([
+      core.claimStackingReward(targetCycle, user1),
+      core.claimStackingReward(targetCycle, user2),
+      core.claimStackingReward(targetCycle, user3),
+    ]);
+    // assert
+    block.receipts[0].result.expectOk().expectBool(true);
+    block.receipts[1].result.expectOk().expectBool(true);
+    block.receipts[2].result.expectOk().expectBool(true);
+  });
+
+  it("claim-stacking-reward() succeeds in v1 with current reward cycle after shutdown and returns CityCoins without returning STX", () => {
+    // this confirms a known bug in `get-entitled-stacking-reward`
+    // arrange
+    const targetCycle = 16;
+    // act
+    const block = chain.mineBlock([
+      core.claimStackingReward(targetCycle, user2)
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 1);
+    receipt.events.expectFungibleTokenTransferEvent(
+      1000,
+      core.address,
+      user2.address,
+      "miamicoin"
+    );
+  });
+
+  it("claim-stacking-reward() succeeds in v1 with current reward cycle after shutdown and returns CityCoins and STX", () => {
+    // this mitigates a known bug in `get-entitled-stacking-reward`
+    // arrange
+    const targetCycle = 16;
+    // proceed to cycle 17 (60197)
+    chain.mineEmptyBlockUntil(60500);
+    // act
+    const block = chain.mineBlock([
+      core.claimStackingReward(targetCycle, user2)
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 2);
+    receipt.events.expectFungibleTokenTransferEvent(
+      1000,
+      core.address,
+      user2.address,
+      "miamicoin"
+    );
+    receipt.events.expectSTXTransferEvent(
+      3500,
+      core.address,
+      user2.address
+    );
+  });
+
+  it("claim-stacking-reward() succeeds in v1 with future reward cycle after shutdown and returns CityCoins", () => {
+    // arrange
+    const targetCycle = 20;
+    // act
+    const block = chain.mineBlock([
+      core.claimStackingReward(targetCycle, user3)
+    ]);
+    const receipt = block.receipts[0];
+    // assert
+    receipt.result.expectOk().expectBool(true);
+    assertEquals(receipt.events.length, 1);
+    receipt.events.expectFungibleTokenTransferEvent(
+      1000,
+      core.address,
+      user3.address,
+      "miamicoin"
+    );
+  });
+  
 });
 
 run();
-
-/*
-
-it("register-user() succeeds and sets correct coinbase amounts");
-
-it("mine-tokens() fails in v1 with ERR_CONTRACT_NOT_ACTIVATED after upgrade");
-it("mine-tokens() fails in v2 with ERR_CONTRACT_NOT_ACTIVATED if called before initalized")
-it("mine-tokens() succeeds in v2 contract after upgrade and intialization");
-
-it("mine-many() fails in v1 with ERR_CONTRACT_NOT_ACTIVATED after upgrade");
-it("mine-many() fails in v2 with ERR_CONTRACT_NOT_ACTIVATED if called before initalized")
-it("mine-many() succeeds in v2 contract after upgrade and intialization");
-
-// also eval token split, cycle status, etc
-it("stack-tokens() succeeds in v2 contract starting at cycle X");
-it("stack-tokens() succeeds in v2 contract after cycle X");
-
-it("claim-mining-reward() fails in v1 with ERR_CLAIM_IN_WRONG_CONTRACT after shutdown block");
-it("claim-mining-reward() succeeds in v1 with block height in the past");
-
-it("claim-stacking-reward() succeeds in v1 with reward cycle in the past");
-// known issue below, will not return uSTX
-it("claim-stacking-reward() succeeds in v1 with current reward cycle during shutdown");
-it("claim-stacking-reward() succeeds in v1 with reward cycle in the future");
-
-*/
