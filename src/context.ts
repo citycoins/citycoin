2aece1bfbbb03fffe5dec795575dd6cd22bfd382
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-10-13 07:19:20 -0700

    chore: upgrade deps and investigate Clarinet OOM

--- src/context.ts
+++ src/context.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 import { Account, Chain, Tx } from "../deps.ts";
 import { Models } from "./model.ts";
 
@@ -10,10 +11,12 @@ export class Context {
   readonly models: Models;
   readonly deployer: Account;
 
-  constructor(preSetupTx?: Array<Tx>) {
-    (Deno as any).core.ops();
+  constructor(_preSetupTx?: Array<Tx>) {
+    // tests are failing with this included
+    // error: TypeError: Deno.core.ops is not a function
+    // (Deno as any).core.ops();
 
-    let result = JSON.parse(
+    const result = JSON.parse(
       (Deno as any).core.opSync("api/v1/new_session", {
         name: "test",
         loadDeployment: true,
@@ -22,16 +25,14 @@ export class Context {
     );
     this.chain = new Chain(result["session_id"]);
     this.accounts = new Map();
-    for (let account of result["accounts"]) {
+    for (const account of result["accounts"]) {
       this.accounts.set(account.name, account);
     }
     this.contracts = new Map();
-    for (let contract of result["contracts"]) {
+    for (const contract of result["contracts"]) {
       this.contracts.set(contract.contract_id, contract);
     }
-
     this.deployer = this.accounts.get("deployer")!;
-
     this.models = new Models(this.chain, this.deployer);
   }
 
